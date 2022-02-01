3d93007daf9f8302250c05d1ae73266c08a12bf6
Debayan Deb <debayan@lenscorp.ai>
2022-02-01 19:09:29 +0530

    first commit

--- nntools_backup/tensorflow/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import utils
-from . import watcher
-from . import tensor_ops
-from . import image_ops
-from . import losses
-
-# sub-packages
-from . import networks
-
--- nntools_backup/tensorflow/networks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .basenet import BaseNetwork
-from .binary_cnn import JointCNN
-from .chimney_cnn import ChimneyCNN
